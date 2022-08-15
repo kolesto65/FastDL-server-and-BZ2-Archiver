d71dae5b2f018782eef20aad91e05b60dc37ff4b
kolesto65 <kolesto65@gmail.com>
2022-08-15 05:25:51 +0300

    Beautified fastdl.js 🥺

--- fastdl.js
+++ fastdl.js
@@ -8,64 +8,64 @@ var files = require('./files.json')
 var mainpath = path.resolve(settings.csgo_dir)
 var dlpath = path.resolve(settings.fastdl_dir)
 app.get('/update', async (req, res) => {
-  var total = ''
-  if(!req.query) return res.sendStatus(403)
-    if(!req.query.pass) return res.sendStatus(403)
-      if(req.query.pass !== settings.password) return res.sendStatus(403)
-      if(req.query.pass == settings.password) {
+    var total = ''
+    if (!req.query) return res.sendStatus(403)
+    if (!req.query.pass) return res.sendStatus(403)
+    if (req.query.pass !== settings.password) return res.sendStatus(403)
+    if (req.query.pass == settings.password) {
 
         var walk = function(dir, done) {
-  fs.readdir(dir, function(err, list) {
-    if (err) return done(err);
-    var results = []
-    var processed = 0
-    var i = 0;
-    (function next() {
-      var file = list[i++];
-      if (!file) return done(null, results);
-      file = path.resolve(dir, file);
-      fs.stat(file, async function(err, stat) {
-        if (stat && stat.isDirectory()) {
-          try {
- await fs.mkdirSync(settings.fastdl_dir+'/'+file.slice(mainpath.length+1));
-} catch (error) {
-  if(error.errno == -17) console.log(`Skipping existing directory (${error.path})`)
-  else console.error(error);
-}
-          walk(file, function(err, res) {
-            results = results.concat(res);
-            next();
-          });
-        } else {
-          if(settings.extensions.find(x=> x == path.extname(file))) {
-            results.push(file);
-            processed += 1
-                        console.log(`Processing: ${file} 
+            fs.readdir(dir, function(err, list) {
+                if (err) return done(err);
+                var results = []
+                var processed = 0
+                var i = 0;
+                (function next() {
+                    var file = list[i++];
+                    if (!file) return done(null, results);
+                    file = path.resolve(dir, file);
+                    fs.stat(file, async function(err, stat) {
+                        if (stat && stat.isDirectory()) {
+                            try {
+                                await fs.mkdirSync(settings.fastdl_dir + '/' + file.slice(mainpath.length + 1));
+                            } catch (error) {
+                                if (error.errno == -17) console.log(`Skipping existing directory (${error.path})`)
+                                else console.error(error);
+                            }
+                            walk(file, function(err, res) {
+                                results = results.concat(res);
+                                next();
+                            });
+                        } else {
+                            if (settings.extensions.find(x => x == path.extname(file))) {
+                                results.push(file);
+                                processed += 1
+                                console.log(`Processing: ${file} 
               Total: ${processed}`)
-            total += file+'<br/>'
-            if(files.indexOf(file) == -1) {
-              files.push(file)
-              await exec.execSync(`bzip2 -ckz "${file}" > "${settings.fastdl_dir+'/'+file.slice(mainpath.length+1)}.bz2"`)
-            }
-          }
-          next();
-        }
-      });
-    })();
-  });
-};
+                                total += file + '<br/>'
+                                if (files.indexOf(file) == -1) {
+                                    files.push(file)
+                                    await exec.execSync(`bzip2 -ckz "${file}" > "${settings.fastdl_dir+'/'+file.slice(mainpath.length+1)}.bz2"`)
+                                }
+                            }
+                            next();
+                        }
+                    });
+                })();
+            });
+        };
 
-walk(settings.csgo_dir, function(err, results) {
-  if (err) throw err;
-  fs.writeFileSync('files.json', JSON.stringify(files, null, '\t'));
-  return res.send(`Files: ${results.length}<br/>${total}`)
-  console.log(`Done! ${results.length} files`)
-});
-      }
+        walk(settings.csgo_dir, function(err, results) {
+            if (err) throw err;
+            fs.writeFileSync('files.json', JSON.stringify(files, null, '\t'));
+            return res.send(`Files: ${results.length}<br/>${total}`)
+            console.log(`Done! ${results.length} files`)
+        });
+    }
 })
 
 app.use(express.static(settings.fastdl_dir))
 
 app.listen(settings.port, () => {
-  console.log(`FastDL server started on port: ${settings.port}`)
+    console.log(`FastDL server started on port: ${settings.port}`)
 })
